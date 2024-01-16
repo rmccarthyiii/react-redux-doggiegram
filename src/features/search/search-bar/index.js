cb74bbef9d9d6d0cd845318d0f673001a38e478d
Robert McCarthy <romccarthy@indeed.com>
2024-01-16 02:25:25 -0600

    Update formatting of files

--- src/features/search/search-bar/index.js
+++ src/features/search/search-bar/index.js
@@ -3,26 +3,26 @@ import { selectSearchTerm, setSearchTerm } from '../search.slice';
 import './search-bar.css';
 
 export default function SearchBar() {
-  const searchTerm = useSelector(selectSearchTerm);
-  // Task 10: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
-  const dispatch = useDispatch();
+    const searchTerm = useSelector(selectSearchTerm);
+    // Task 10: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+    const dispatch = useDispatch();
 
-  function handleChange({ target: { value } }) {
-    // Task 11: Dispatch the `setSearchTerm()` action creator, passing in the value of the search input
-    dispatch(setSearchTerm(value));
-  }
+    function handleChange({ target: { value } }) {
+        // Task 11: Dispatch the `setSearchTerm()` action creator, passing in the value of the search input
+        dispatch(setSearchTerm(value));
+    }
 
-  return (
-    <form className="search-bar">
-      <label htmlFor="search">Search by caption: </label>
-      <input
-        id="search"
-        name="search"
-        onChange={handleChange}
-        placeholder="e.g., terrier"
-        type="search"
-        value={searchTerm}
-      />
-    </form>
-  );
+    return (
+        <form className="search-bar">
+            <label htmlFor="search">Search by caption: </label>
+            <input
+                id="search"
+                name="search"
+                onChange={handleChange}
+                placeholder="e.g., terrier"
+                type="search"
+                value={searchTerm}
+            />
+        </form>
+    );
 }
